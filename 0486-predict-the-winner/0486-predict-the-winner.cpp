88f4aa1de3b81927420cfd61c6bd491940e1fa21
Tanisha Verma <tanishaverma2305@gmail.com>
2026-08-02 01:48:51 +0530

    Time: 91 ms (22.45%), Space: 9.5 MB (85.04%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int solve(vector<int>& nums, int i, int j) {
 
-        // Only one element left
         if (i == j)
             return nums[i];
 
-        // Take left
         int left = nums[i] - solve(nums, i + 1, j);
 
-        // Take right
         int right = nums[j] - solve(nums, i, j - 1);
 
-        // Choose the better option
         return max(left, right);
     }
 

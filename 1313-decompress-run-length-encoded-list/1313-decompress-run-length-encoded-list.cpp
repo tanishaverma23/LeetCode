5a929b5f59322a53f3d240d9eda2e2da7595b589
Tanisha Verma <tanishaverma2305@gmail.com>
2026-08-10 03:15:46 +0530

    Time: 0 ms (100%), Space: 13 MB (93.83%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size()-1;i+=2){
+            int freq=nums[i];
+            int val=nums[i+1];
+              while(freq!=0){
+                ans.push_back(val);
+                freq--;
+              }
+        }
+        return ans;
+    }
+};

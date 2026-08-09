b3209781c940fdee6b2f898f0d83b9860c394870
Tanisha Verma <tanishaverma2305@gmail.com>
2026-08-10 03:01:56 +0530

    Time: 0 ms (100%), Space: 13.1 MB (52.07%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxi = INT_MIN;
+        for (int i = 0; i < sentences.size(); i++) {
+            int cnt = 0;
+            for (int j = 0; j < sentences[i].size(); j++) {
+                if (sentences[i][j] == ' ') {
+                    cnt++;
+                }
+            }
+            maxi = max(maxi, cnt + 1);
+        }
+
+        return maxi;
+    }
+};

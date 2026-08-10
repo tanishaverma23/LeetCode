d4f5f8dd44a614ff9705480d12cbc2eb43f651f2
Tanisha Verma <tanishaverma2305@gmail.com>
2026-08-10 14:32:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (12.44%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin=celsius + 273.15;
+        double fahr=celsius * 1.80 + 32.00;
+
+        vector<double>ans;
+       ans.push_back(kelvin);
+       ans.push_back(fahr);
+
+
+        return ans;
+    }
+};

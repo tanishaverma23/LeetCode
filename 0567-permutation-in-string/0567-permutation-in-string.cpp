9a2ed6f319fed537da5c4fd8a295351e4e231512
Tanisha Verma <tanishaverma2305@gmail.com>
2026-08-15 13:25:41 +0530

    Time: 0 ms (100%), Space: 10 MB (70.96%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length())
-                   return false;
-        vector<int> freq(26, 0);
-        for (int i = 0; i < s1.length(); i++) {
-            freq[s1[i] - 'a']++;
-        }
-        int k = s1.size();
 
-        for (int l = 0; l <= s2.length() - k; l++) {
-            vector<int> temp(freq.begin(), freq.end());
-            int cnt = k;
-            int r = l;
-            while (cnt>0 && temp[s2[r] - 'a'] > 0) {
-                temp[s2[r] - 'a']--;
-                cnt--;
-                r++;
+        int k = s1.length();
+
+        if (k > s2.length())
+            return false;
+
+        vector<int> freq(26, 0);
+        vector<int> window(26, 0);
+
+        for (char c : s1)
+            freq[c - 'a']++;
+
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+
+            window[s2[right] - 'a']++;
+
+        
+            if (right - left + 1 > k) {
+                window[s2[left] - 'a']--;
+                left++;
             }
-            if (cnt== 0) {
-                return true;
+
+         
+            if (right - left + 1 == k) {
+                if (freq == window)
+                    return true;
             }
-           
-            
-          
         }
+
         return false;
     }
 };

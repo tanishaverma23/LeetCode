152275131737405da7633c37bb6252b85bf540b8
Tanisha Verma <tanishaverma2305@gmail.com>
2026-09-04 18:02:33 +0530

    Time: 0 ms (100%), Space: 31.1 MB (56.73%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> minSuffix(n);
+        minSuffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            minSuffix[i] = min(nums[i], minSuffix[i + 1]);
+        }
+        int maxPrefix = nums[0];
+        for (int i = 0; i < n; ++i) {
+            maxPrefix = max(maxPrefix, nums[i]);
+            
+     
+            if (maxPrefix - minSuffix[i] <= k) {
+                return i; 
+            }
+        }
+        
+        return -1;
+    }
+};
